3ff6ea1e5cba6b2d3f85877157e05b619d24286a
ayomidealabi <ayomidealabi2@gmail.com>
2019-11-01 15:02:32 +0100

    first commit

---
+++ classjs/js/javascript.js
@@ -0,0 +1,55 @@
+//This code displays 'Hello World'
+document.write('<strong>Hello World</strong><br>'); 
+document.write('<strong>I am here</strong><br>'); 
+document.write('<strong>It is a good day</strong><br>'); 
+
+//This code displays my name
+document.write('<strong>Ayomide Alabi</strong><br>'); 
+
+/*This is how I write  my codes I ca
+show people everything about my codes*/
+
+
+let first_name = 'Ayomide';
+let last_name = 'Alabi';
+console.log(first_name);
+
+// first_name = 'Kevin'
+// console.log(first_name);
+
+let email ='johndoe';
+
+let school ='alabian';
+
+// document.write(email '<br>');
+
+// let ask = 'My name is'
+
+// document.write( ask + first_name  + last_name);
+
+// let add='I live  at'
+
+// let add2='Oluakerele street'
+
+// document.write(add + 3 + add2)
+
+let name= 'Josh'
+
+let add= 'akinrele'
+
+let street_no='3'
+
+console.log(`My name is ${name} I live at ${street_no} ${add}`)
+
+let x='3', y='10', z='20', a='5', u;
+
+document.write(u= x * y + z / a);
+
+let c="3"
+
+let c2=3
+
+console.log(c - c2)
+
+
+
